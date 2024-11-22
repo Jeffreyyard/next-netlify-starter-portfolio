215af7a0d025de7c4d52c95f7017102b44a9cc19
Jeffreyyard <30838061+Jeffreyyard@users.noreply.github.com>
2024-11-21 20:56:59 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Jeffrey Yard!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Back to start or Do it again <code>pages/index.js</code>
         </p>
       </main>
 
